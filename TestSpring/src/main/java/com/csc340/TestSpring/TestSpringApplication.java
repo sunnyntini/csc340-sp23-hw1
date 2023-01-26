1bdfccc6938e719cff73534f6e9bf911def25560
j_calca2 <j_calca2@uncg.edu>
2023-01-25 22:55:44 -0500

    Joe Calcagno adding to the readme file

---
+++ TestSpring/src/main/java/com/csc340/TestSpring/TestSpringApplication.java
@@ -0,0 +1,13 @@
+package com.csc340.TestSpring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestSpringApplication.class, args);
+	}
+
+}
---
+++ TestSpring/src/test/java/com/csc340/TestSpring/TestSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.csc340.TestSpring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
